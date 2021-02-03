341e5ce003e209898f477c645440d23748a8058e
DanielC <defenderchin@gmail.com>
2021-02-04 00:07:22 +0800

    fixing collectstatic error

--- back/settings.py
+++ back/settings.py
@@ -184,11 +184,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "front/build/static"),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 MEDIA_ROOT = 'uploads/'
 MEDIA_URL = '/media/'
